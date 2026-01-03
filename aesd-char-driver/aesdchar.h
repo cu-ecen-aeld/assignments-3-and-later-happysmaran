9e8392048cbc4bfe552c307a36a8d3296f648269
Smaran Vallabhaneni <smaranvallabhaneni@gmail.com>
2026-01-03 08:39:01 +0000

    even more ass. 8 files
    blud forgot to update aesdchar.h

--- aesd-char-driver/aesdchar.h
+++ aesd-char-driver/aesdchar.h
@@ -8,20 +8,13 @@
 #ifndef AESD_CHAR_DRIVER_AESDCHAR_H_
 #define AESD_CHAR_DRIVER_AESDCHAR_H_
 
-#define AESD_DEBUG 1  //Remove comment on this line to enable debug
+#include <linux/mutex.h>           // For struct mutex
+#include <linux/cdev.h>            // For struct cdev
+#include "aesd-circular-buffer.h"  // For struct aesd_circular_buffer
 
-#undef PDEBUG             /* undef it, just in case */
-#ifdef AESD_DEBUG
-#  ifdef __KERNEL__
-     /* This one if debugging is on, and kernel space */
-#    define PDEBUG(fmt, args...) printk( KERN_DEBUG "aesdchar: " fmt, ## args)
-#  else
-     /* This one for user space */
-#    define PDEBUG(fmt, args...) fprintf(stderr, fmt, ## args)
-#  endif
-#else
-#  define PDEBUG(fmt, args...) /* not debugging: nothing */
-#endif
+#define AESD_DEBUG 1  
+
+// ... (Your PDEBUG macros stay here) ...
 
 struct aesd_dev
 {
@@ -29,8 +22,13 @@ struct aesd_dev
     char *partial_entry_ptr;
     size_t partial_entry_size;
     struct mutex lock;
-    struct cdev cdev;     /* Char device structure      */
+    struct cdev cdev;
 };
 
+// Add these prototypes so the compiler knows the signatures for the fops
+int aesd_open(struct inode *inode, struct file *filp);
+int aesd_release(struct inode *inode, struct file *filp);
+ssize_t aesd_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos);
+ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos);
 
 #endif /* AESD_CHAR_DRIVER_AESDCHAR_H_ */
